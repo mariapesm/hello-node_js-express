43aea828fa15c9958f83062c59663186b0512afc
mariapesm <youandmecanyousee@hotmail.com>
2018-11-30 23:24:47 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,21 +1,8 @@
-var express = require("express");
-var app     = express();
-var path    = require("path");
+var express = require('express');
+var app = express();
 
-
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+app.get('/', function (req, res) {
+  res.send('hello, you see it because it works a request api');
 });
 
-app.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
-});
-
-app.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
-});
-
-app.listen(3000);
-
-console.log("Running at Port 3000");
+app.listen(process.env.PORT || 3000);
