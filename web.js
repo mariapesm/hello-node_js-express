47b4c5ca96a3de00220ce3fdd61601af2f2bef8a
mariapesm <youandmecanyousee@hotmail.com>
2018-11-30 23:55:43 +0200

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello, you see it because it works a request api');
+  res.sendFile('index.html');
 });
 
 app.listen(process.env.PORT || 3000);
