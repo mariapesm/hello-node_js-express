323ae5065f67d119368f3f20c5e1e4388c5381ff
mariapesm <youandmecanyousee@hotmail.com>
2018-12-01 00:41:21 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-
+var https = require('https');
+var http = require('http');
 app.get('/', function (req, res) {
- res.sendFile( __dirname + "index1.html" );
+ res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(process.env.PORT || 3000);
